e4a1609943d82007232c89ad4078ac0e0bd8dc7a
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-02-27 13:37:50 +0100

    2nd commit

--- src/main/java/com/example/githubactionsspringbootd/controller/WelcomeController.java
+++ src/main/java/com/example/githubactionsspringbootd/controller/WelcomeController.java
@@ -6,4 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
     @GetMapping("")
+    public String index(){
+        return "welcome";
+    }
 }
