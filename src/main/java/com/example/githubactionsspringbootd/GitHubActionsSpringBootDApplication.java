6d92286d38f57c2419b78807ef0643f67ae60d53
UsmanSharif0001 <ussh0001@kea.stud.dk>
2024-02-27 13:27:27 +0100

    Initial commit

---
+++ src/main/java/com/example/githubactionsspringbootd/GitHubActionsSpringBootDApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsspringbootd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsSpringBootDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsSpringBootDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubactionsspringbootd/GitHubActionsSpringBootDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionsspringbootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsSpringBootDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
